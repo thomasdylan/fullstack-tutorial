826b61666216da80083be7a7a081ec0bfd752dc4
Dylan Thomas <thomasdylan76@gmail.com>
2020-03-19 16:33:52 -0600

    Lesson 3

---
+++ start/server/src/index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const { createStore } = require('./utils');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({ 
+    typeDefs,
+    dataSources: () => ({
+        LaunchAPI: new LaunchAPI(),
+        UserAPI: new UserAPI({ store })
+    })
+});
+
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+});
